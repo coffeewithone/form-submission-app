602ceae4b9e67d5c99b1446eaefea1cb0a877864
coffeewithone <coffeeandone@gmail.com>
2023-04-21 02:53:54 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
